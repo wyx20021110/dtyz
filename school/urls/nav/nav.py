8bf78f8148e1ef2cefb9244449050f056ca6ada8
wyx <wyx20021110@gmail.com>
2022-08-13 18:21:35 +0800

    lue

--- school/urls/nav/nav.py
+++ school/urls/nav/nav.py
@@ -3,5 +3,5 @@ from school.views.nav.nav import about, news
 
 urlpatterns = [
     path('about/', about, name='about'),
-    path('news', news, name="news")
+    path('news/', news, name="news"),
 ]
